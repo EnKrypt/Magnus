7c2814c6a02aba38e999108bcc2e92df4ada1fab
Adithya Reddy <adithyakreddy6@gmail.com>
2017-02-17 21:13:30 +0530

    Header syntax

--- app.js
+++ app.js
@@ -11,8 +11,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Methods", "GET, POST");
+	res.set({
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'GET, POST'
+	});
+
 	next();
 });
 
