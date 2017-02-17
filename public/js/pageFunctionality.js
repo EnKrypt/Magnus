c1373392590ab0df17eedc430cae0dfcb8d9e80b
Arvind Kumar <arvind@enkrypt.in>
2017-02-17 22:23:57 +0530

    Refactored with Jquery shorthands

--- public/js/pageFunctionality.js
+++ public/js/pageFunctionality.js
@@ -1,14 +1,11 @@
 let queue = [];
 
 function getQueue() {
-    $.ajax({
-      url: 'https://arvind.io:8085/getqueue',
-      success: result => {
-          queue = result;
-      }
+    $.get('https://arvind.io:8085/getqueue', result => {
+        queue = JSON.parse(result);
     });
 }
 
-$(document).ready(() => {
+$(() => {
     setInterval(getQueue, 200);
 });
