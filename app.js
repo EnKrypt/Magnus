ef4f7494002efa0d5adcaefca238ada28c190060
Arvind Kumar <arvind@enkrypt.in>
2017-02-17 20:54:56 +0530

    Added base express project

---
+++ app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var express = require('express'),
+    bodyParser = require("body-parser");
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static(path.join(application_root, "public")));
+app.use(function(req, res, next){
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET, POST");
+	next();
+});
+
+app.get('/', function(req, res) {
+    res.send('Magnus is running');
+})
+
+app.listen(8081);
