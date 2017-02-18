93e48b5f45812a0c7845465201626ff1ca9c3f3b
Adithya Reddy <adithyakreddy6@gmail.com>
2017-02-19 01:51:13 +0530

    First element green part 2

--- public/js/pageFunctionality.js
+++ public/js/pageFunctionality.js
@@ -7,7 +7,7 @@ function getQueue() {
         $('#queuecontents').html('');
 
         for (let key in queue) {
-            $('#queuecontents').append('<div class="well"><h4>Priority: ' + queue[key].priority + '</h4><strong>ID</strong>: ' + queue[key].beaconid + '<br><strong>From</strong>: ' + queue[key].from + ' <strong>To</strong>: ' + queue[key].to + '</div>');
+            $('#queuecontents').append('<div class="well queueitem"><h4>Priority: ' + queue[key].priority + '</h4><strong>ID</strong>: ' + queue[key].beaconid + '<br><strong>From</strong>: ' + queue[key].from + ' <strong>To</strong>: ' + queue[key].to + '</div>');
             $("#queuecontents").first().css("background-color", "dark-green");
             console.log(atFloor);
         }
