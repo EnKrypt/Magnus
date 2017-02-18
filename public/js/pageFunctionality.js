67531ab82008f001477eeccc4aa822372d5bde4a
Arvind Kumar <arvind@geektrust.in>
2017-02-18 15:12:26 +0530

    Added view compartments and displaying current queue

--- public/js/pageFunctionality.js
+++ public/js/pageFunctionality.js
@@ -3,6 +3,7 @@ let queue = [];
 function getQueue() {
     $.get('https://arvind.io:8085/getqueue', result => {
         queue = result;
+        $('.queuecontents').html(JSON.stringify(queue));
     });
 }
 
