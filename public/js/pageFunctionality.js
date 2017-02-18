8ca227f019db9c7e69099772ded119b54512ab61
Adithya Reddy <adithyakreddy6@gmail.com>
2017-02-19 01:29:13 +0530

    Formatting

--- public/js/pageFunctionality.js
+++ public/js/pageFunctionality.js
@@ -7,7 +7,7 @@ function getQueue() {
         $('#queuecontents').html('');
 
         for (let key in queue) {
-            $('#queuecontents').append('<div class="well"><br><h4>Priority</h4>: ' + queue[key].priority + ' <strong>ID</strong>: ' + queue[key].beaconid + '<br><strong>From</strong>: ' + queue[key].from + ' <strong>To</strong>: ' + queue[key].to + '</div>');
+            $('#queuecontents').append('<div class="well"><h4>Priority: ' + queue[key].priority + '</h4><br><strong>ID</strong>: ' + queue[key].beaconid + '<br><strong>From</strong>: ' + queue[key].from + ' <strong>To</strong>: ' + queue[key].to + '</div>');
             console.log(atFloor);
         }
     });
