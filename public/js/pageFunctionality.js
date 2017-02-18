6c354bdbd44d03740bf243d16e211e7ae2d4ec32
Adithya Reddy <adithyakreddy6@gmail.com>
2017-02-19 02:39:31 +0530

    Elevator functionality attempt 2

--- public/js/pageFunctionality.js
+++ public/js/pageFunctionality.js
@@ -1,4 +1,6 @@
 let queue = [];
+let prevfloor = 0;
+let atFloor;
 
 function getQueue() {
     $.get('https://arvind.io:8085/getqueue', result => {
@@ -12,8 +14,12 @@ function getQueue() {
         }
         $($('.queueitem')[0]).css('background-color', 'green');
         $($('.queueitem')[0]).css('color', 'white');
+
+        $($('#floor' + prevfloor).css('background-color', '#f5f5f5'));
+        $($('#floor' + prevfloor).css('color', 'black'));
         $($('#floor' + atFloor).css('background-color', 'green'));
         $($('#floor' + atFloor).css('color', 'white'));
+        prevfloor = atFloor;
     });
 }
 
