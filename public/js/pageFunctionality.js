ee99d3a07b14f2ce2ad61e1a14d4fcdce9cd137d
Arvind Kumar <arvind@geektrust.in>
2017-02-19 09:40:48 +0530

    otf

--- public/js/pageFunctionality.js
+++ public/js/pageFunctionality.js
@@ -1,4 +1,5 @@
 let queue = [];
+let events = [];
 let prevfloor = 0;
 let atFloor;
 let waiting;
@@ -34,152 +35,155 @@ function getQueue() {
 }
 
 function getGraph() {
-    $.get('https://arvind.io:8085/getevents', events => {
-        var floor = 0;
-        for (var key in events) {
-            events[key].oseconds = Math.round((Math.abs(events[key].from - floor) + 6 + Math.abs(events[key].to - events[key].from)) / (Math.abs(events[key].from - events[key].to)) * 10) / 10;
-            floor = events[key].to;
-        }
-
-        floor = 0;
-        events = events.sort(function (a, b) {
-            return b.priority - a.priority;
-        });
-        for (var key in events) {
-            events[key].pseconds = Math.round((Math.abs(events[key].from - floor) + 6 + Math.abs(events[key].to - events[key].from)) / (Math.abs(events[key].from - events[key].to)) * 10) / 10;
-            floor = events[key].to;
-        }
-
-        traditionalData = events.map(function(obj) {
-            return obj.oseconds;
-        });
-
-        optimizedData = events.map(function(obj) {
-            return obj.pseconds;
-        });
-
-        console.log(traditionalData);
-        console.log(optimizedData);
-
-        let tchart = $("#traditionalChart");
-        let ochart = $("#optimizedChart");
-
-        var traditionalChartdata = {
-            labels: events.map(function(obj) {
-                return obj.priority + '';
-            }),
-            datasets: [
-                {
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1,
-                    data: traditionalData
-                }
-            ]
-        };
-
-        var oChartdata = {
-            labels: events.map(function(obj) {
-                return obj.priority + '';
-            }),
-            datasets: [
-                {
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1,
-                    data: optimizedData
-                }
-            ]
-        };
-        let chartt = new Chart(tchart, {
-            type: 'bar',
-            data: traditionalChartdata,
-            options: {
-                scales: {
-                    yAxes: [{
-                        scaleLabel: {
-                            display: true,
-                            labelString: "Seconds (Normalized)"
-                        },
-                        display: true,
-                        ticks: {
-                            min: 0,
-                            max: 5
-                        }
-                    }],
-                    xAxes: [{
-                        scaleLabel: {
-                            display: true,
-                            labelString: "Priority"
-                        }
-                    }],
-                },
-                legend: {
-                    display: false
-                }
+    $.get('https://arvind.io:8085/getevents', eventslist => {
+        if (events.length != eventslist.length) {
+            events = eventslist;
+            var floor = 0;
+            for (var key in events) {
+                events[key].oseconds = Math.round((Math.abs(events[key].from - floor) + 6 + Math.abs(events[key].to - events[key].from)) / (Math.abs(events[key].from - events[key].to)) * 10) / 10;
+                floor = events[key].to;
             }
-        });
 
-        let charto = new Chart(ochart, {
-            type: 'bar',
-            data: oChartdata,
-            options: {
-                scales: {
-                    yAxes: [{
-                        scaleLabel: {
-                            display: true,
-                            labelString: "Seconds (Normalized)"
-                        },
-                        display: true,
-                        ticks: {
-                            min: 0,
-                            max: 5
-                        }
-                    }],
-                    xAxes: [{
-                        scaleLabel: {
-                            display: true,
-                            labelString: "Priority"
-                        }
-                    }],
-                },
-                legend: {
-                    display: false
-                }
+            floor = 0;
+            events = events.sort(function (a, b) {
+                return b.priority - a.priority;
+            });
+            for (var key in events) {
+                events[key].pseconds = Math.round((Math.abs(events[key].from - floor) + 6 + Math.abs(events[key].to - events[key].from)) / (Math.abs(events[key].from - events[key].to)) * 10) / 10;
+                floor = events[key].to;
             }
-        });
+
+            traditionalData = events.map(function(obj) {
+                return obj.oseconds;
+            });
+
+            optimizedData = events.map(function(obj) {
+                return obj.pseconds;
+            });
+
+            console.log(traditionalData);
+            console.log(optimizedData);
+
+            let tchart = $("#traditionalChart");
+            let ochart = $("#optimizedChart");
+
+            var traditionalChartdata = {
+                labels: events.map(function(obj) {
+                    return obj.priority + '';
+                }),
+                datasets: [
+                    {
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1,
+                        data: traditionalData
+                    }
+                ]
+            };
+
+            var oChartdata = {
+                labels: events.map(function(obj) {
+                    return obj.priority + '';
+                }),
+                datasets: [
+                    {
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1,
+                        data: optimizedData
+                    }
+                ]
+            };
+            let chartt = new Chart(tchart, {
+                type: 'bar',
+                data: traditionalChartdata,
+                options: {
+                    scales: {
+                        yAxes: [{
+                            scaleLabel: {
+                                display: true,
+                                labelString: "Seconds (Normalized)"
+                            },
+                            display: true,
+                            ticks: {
+                                min: 0,
+                                max: 5
+                            }
+                        }],
+                        xAxes: [{
+                            scaleLabel: {
+                                display: true,
+                                labelString: "Priority"
+                            }
+                        }],
+                    },
+                    legend: {
+                        display: false
+                    }
+                }
+            });
+
+            let charto = new Chart(ochart, {
+                type: 'bar',
+                data: oChartdata,
+                options: {
+                    scales: {
+                        yAxes: [{
+                            scaleLabel: {
+                                display: true,
+                                labelString: "Seconds (Normalized)"
+                            },
+                            display: true,
+                            ticks: {
+                                min: 0,
+                                max: 5
+                            }
+                        }],
+                        xAxes: [{
+                            scaleLabel: {
+                                display: true,
+                                labelString: "Priority"
+                            }
+                        }],
+                    },
+                    legend: {
+                        display: false
+                    }
+                }
+            });
+        }
     });
 }
 
 $(() => {
     setInterval(getQueue, 200);
-    getGraph();
+    setInterval(getGraph, 500);
 });
