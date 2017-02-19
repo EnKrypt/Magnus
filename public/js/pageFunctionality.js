6dccb2626243dfbc24c519b8d8a3ee64e69b0fa2
Arvind Kumar <arvind@geektrust.in>
2017-02-19 05:49:59 +0530

    fix

--- public/js/pageFunctionality.js
+++ public/js/pageFunctionality.js
@@ -35,9 +35,7 @@ function getGraph() {
     $.get('https://arvind.io:8085/getevents', events => {
         var floor = 0;
         for (var key in events) {
-            console.log(Math.abs(events[key].from - floor) + 6 + Math.abs(events[key].to = events[key].from));
-            console.log(events[key].from, events[key].to);
-            events[key].oseconds = (Math.abs(events[key].from - floor) + 6 + Math.abs(events[key].to = events[key].from)) / (Math.abs(events[key].from - events[key].to));
+            events[key].oseconds = (Math.abs(events[key].from - floor) + 6 + Math.abs(events[key].to - events[key].from)) / (Math.abs(events[key].from - events[key].to));
             floor = events[key].to;
         }
 
@@ -46,9 +44,7 @@ function getGraph() {
             return b.priority - a.priority;
         });
         for (var key in events) {
-            console.log(Math.abs(events[key].from - floor) + 6 + Math.abs(events[key].to = events[key].from));
-            console.log(events[key].from, events[key].to);
-            events[key].pseconds = (Math.abs(events[key].from - floor) + 6 + Math.abs(events[key].to = events[key].from)) / (Math.abs(events[key].from - events[key].to));
+            events[key].pseconds = (Math.abs(events[key].from - floor) + 6 + Math.abs(events[key].to - events[key].from)) / (Math.abs(events[key].from - events[key].to));
             floor = events[key].to;
         }
         console.log(events.map(function(obj) {
