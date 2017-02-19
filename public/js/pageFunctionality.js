599ae3646379d5287bbb7c5b5905c59d33739511
Adithya Reddy <adithyakreddy6@gmail.com>
2017-02-19 06:27:01 +0530

    charts!

--- public/js/pageFunctionality.js
+++ public/js/pageFunctionality.js
@@ -64,14 +64,91 @@ function getGraph() {
         let tchart = $("#traditionalChart");
         let ochart = $("#optimizedChart");
 
+        var traditionalChartdata = {
+            labels: events.map(function(obj) {
+                return obj.priority + '';
+            }),
+            datasets: [
+                {
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1,
+                    data: traditionalData
+                }
+            ]
+        };
+
+        var oChartdata = {
+            labels: events.map(function(obj) {
+                return obj.priority + '';
+            }),
+            datasets: [
+                {
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1,
+                    data: optimizedData
+                }
+            ]
+        };
         let chartt = new Chart(tchart, {
             type: 'bar',
-            data: traditionalData
+            data: traditionalChartdata,
+            options: {
+                scales: {
+                    yAxes: [{
+                        display: true,
+                        ticks: {
+                            min: 0,
+                            max: 5
+                        }
+                    }]
+                }
+            }
         });
 
         let charto = new Chart(ochart, {
             type: 'bar',
-            data: optimizedData
+            data: oChartdata,
+            options: {
+                scales: {
+                    yAxes: [{
+                        display: true,
+                        ticks: {
+                            min: 0,
+                            max: 5
+                        }
+                    }]
+                }
+            }
         });
     });
 }
